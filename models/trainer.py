70f3693debe79cae7ea1085c4b202b6e8a2ebfaf
Your Name <you@example.com>
2024-02-20 07:17:24 +0800

    update trainer

--- models/trainer.py
+++ models/trainer.py
@@ -1,3 +1,20 @@
 import torch
 import torch.nn as nn
 import lightning as L
+
+class RegionClip(L.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+    def training_step(self):
+        pass
+
+    def validation_step(self):
+        pass
+
+    def configure_optimizers(self):
+        pass
+    @property
+    def trainer_arguments(self):
+        """Set model-specific trainer arguments."""
+        return {}
