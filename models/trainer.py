8070a04e97e07df870c3a19df6c473d07d2e8830
Your Name <you@example.com>
2024-02-29 16:49:56 +0800

    update scheduler

--- models/trainer.py
+++ models/trainer.py
@@ -100,7 +100,7 @@ class RegionClip(L.LightningModule):
             lr=1e-3
         )
         scheduler = optim.lr_scheduler.ExponentialLR(
-            optimizer, 0.1, last_epoch=-1, verbose=True)
+            optimizer, 0.8, last_epoch=-1, verbose=True)
         return {"optimizer": optimizer,"lr_scheduler": scheduler}
 
 
